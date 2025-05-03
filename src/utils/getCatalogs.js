d598f09f5c198f0871f5b3d09981119e39271be3
Stefan Wajda <audyt@lepszyweb.pl>
2025-05-03 22:45:47 +0200

    dodaj brakiujący wiersz

--- src/utils/getCatalogs.js
+++ src/utils/getCatalogs.js
@@ -4,6 +4,7 @@ import wcag22catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-en.yaml
 import wcag22eucatalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-eu-en.yaml";
 import wcag21catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.1-en.yaml";
 import wcag21508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.1-508-en.yaml";
+export const wcag20508catalogName = "2.5-edition-wcag-2.0-508-en";
 export const wcag22508catalogName = "2.5-edition-wcag-2.2-508-en";
 export const wcag22catalogName = "2.5-edition-wcag-2.2-en";
 export const wcag21catalogName = "2.5-edition-wcag-2.1-en";
