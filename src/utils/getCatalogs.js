9f7407fedbeac6eb35dc3e45ca8f417d8a4251c3
Stefan Wajda <audyt@lepszyweb.pl>
2025-05-03 18:43:14 +0200

    sprawdz blad 3

--- src/utils/getCatalogs.js
+++ src/utils/getCatalogs.js
@@ -1,23 +1,25 @@
 import wcag20508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.0-508-en.yaml";
 // import wcag22508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-508-en.yaml";
 // import wcag22catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-en.yaml";
-import wcag22301catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-508-eu-en.yaml";
+// import wcag22301catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-508-eu-en.yaml";
 import wcag21catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.1-en.yaml";
 import wcag21508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.1-508-eu-en.yaml";
 export const wcag20508catalogName = "2.5-edition-wcag-2.0-508-en";
 // const wcag22508catalogName = "2.5-edition-wcag-2.2-508-en";
 // const wcag22catalogName = "2.5-edition-wcag-2.2-en";
 const wcag21catalogName = "2.5-edition-wcag-2.1-en";
-const wcag22301catalogName = "2.5-edition-wcag-2.2-508-eu-en";
+// const wcag22301catalogName = "2.5-edition-wcag-2.2-508-eu-en";
 const wcag21508catalogName = "2.5-edition-wcag-2.1-508-en";
 
 
 export function getCatalog(catalogName) {
   if (catalogName == wcag20508catalogName) {
     return wcag20508catalog;
-  } else if (catalogName == wcag21catalogName) {
+  }
+  else if (catalogName == wcag21catalogName) {
     return wcag21catalog;
-  } else if (catalogName == wcag21508catalogName) {
+  }
+  else if (catalogName == wcag21508catalogName) {
       return wcag21508catalog;
 }
     //  else if (catalogName == wcag22508catalogName) {
@@ -26,9 +28,9 @@ export function getCatalog(catalogName) {
     //  else if (catalogName == wcag22catalogName) {
       //     return wcag22catalog;
   /// } 
-  else if (catalogName == wcag22301catalogName) {
-      return wcag22301catalog;
-}
+ //  else if (catalogName == wcag22301catalogName) {
+ //     return wcag22301catalog;
+//}
   return wcag20508catalog;
 }
 
@@ -54,10 +56,10 @@ export function getListOfCatalogs() {
             catalog: wcag21catalogName,
             title: "VPAT® 2.5 WCAG: WCAG 2.1",
         },
-        {
-            catalog: wcag22301catalogName,
-            title: "VPAT® Rev EU: EN 301 549",
-        },
+//        {
+//            catalog: wcag22301catalogName,
+//            title: "VPAT® Rev EU: EN 301 549",
+//        },
     ];
 }
 
