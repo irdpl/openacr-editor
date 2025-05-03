2dfd6eb542f670bc42ab14f7162f5eed2031ab84
Stefan Wajda <audyt@lepszyweb.pl>
2025-05-03 18:23:07 +0200

    sprawdz błąd

--- src/utils/getCatalogs.js
+++ src/utils/getCatalogs.js
@@ -1,11 +1,11 @@
 import wcag20508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.0-508-en.yaml";
-import wcag22508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-508-en.yaml";
+// import wcag22508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-508-en.yaml";
 import wcag22catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-en.yaml";
 import wcag22301catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.2-508-eu-en.yaml";
 import wcag21catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.1-en.yaml";
 import wcag21508catalog from "@openacr/openacr/catalog/2.5-edition-wcag-2.1-508-eu-en.yaml";
 export const wcag20508catalogName = "2.5-edition-wcag-2.0-508-en";
-const wcag22508catalogName = "2.5-edition-wcag-2.2-508-en";
+// const wcag22508catalogName = "2.5-edition-wcag-2.2-508-en";
 const wcag22catalogName = "2.5-edition-wcag-2.2-en";
 const wcag21catalogName = "2.5-edition-wcag-2.1-en";
 const wcag22301catalogName = "2.5-edition-wcag-2.2-508-eu-en";
@@ -19,8 +19,8 @@ export function getCatalog(catalogName) {
     return wcag21catalog;
   } else if (catalogName == wcag21508catalogName) {
       return wcag21508catalog;
-  } else if (catalogName == wcag22508catalogName) {
-      return wcag22508catalog;
+ // } else if (catalogName == wcag22508catalogName) {
+ //     return wcag22508catalog;
   } else if (catalogName == wcag22catalogName) {
       return wcag22catalog;
   } else if (catalogName == wcag22301catalogName) {
@@ -35,10 +35,10 @@ export function getListOfCatalogs() {
             catalog: wcag20508catalogName,
             title: "VPAT® 2.5 508: Poprawione wydanie Section 508 (WCAG 2.0)",
         },
-        {
-            catalog: wcag22508catalogName,
-            title: "VPAT® 2.5 508 + WCAG: Revised Section 508 Edition (WCAG 2.2)",
-        },
+ //       {
+ //           catalog: wcag22508catalogName,
+ //           title: "VPAT® 2.5 508 + WCAG: Revised Section 508 Edition (WCAG 2.2)",
+ //       },
         {
             catalog: wcag22catalogName,
             title: "VPAT® 2.5 WCAG: WCAG 2.2",
